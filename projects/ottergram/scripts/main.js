eed1fdda148e897ed8ec1362a8688e5bd883b165
CSUFTitan <csuftitan@tuffys-MacBook-Air.local>
2020-02-26 16:09:46 -0800

    adding navigation buttons

--- projects/ottergram/scripts/main.js
+++ projects/ottergram/scripts/main.js
@@ -1,86 +1,120 @@
-var DETAIL_IMAGE_SELECTOR = "[data-image-role=\"target\"]";
-var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]'; 
-var DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
+var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
 var HIDDEN_DETAIL_CLASS = 'hidden-detail';
-var TINY_EFFECT_CLASS = 'is-tiny';
 var ESC_KEY = 27;
+var preButton = document.querySelector("#previous-button");
+var nextButton = document.querySelector("#next-button");
+var currentIndex = 0;
+var thumbLength;  
 
-function setDetails(imageUrl, titleText) { 
-    'use strict';
-    // Code will go here
-    var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR); 
-    detailImage.setAttribute('src', imageUrl);
+function setDetails(imageUrl, titleText) {
+  'use strict';
 
-    var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
-    detailTitle.textContent = titleText;
+  var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+  detailImage.setAttribute('src', imageUrl);
 
+  var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+  detailTitle.textContent = titleText;
 }
 
-function imageFromThumb(thumbnail) {
-    'use strict';
-    return thumbnail.getAttribute('data-image-url');
+function imageFromThumb(thumbnail){
+  'use strict';
+  return thumbnail.getAttribute('data-image-url');
 }
 
-function titleFromThumb(thumbnail) {
-    'use strict';
-    return thumbnail.getAttribute('data-image-title');
+function titleFromThumb(thumbnail){
+  'use strict';
+  var title = thumbnail.getAttribute('data-image-title');
+  console.log(title);  // show log in console to check if it work
+  return title;
+  // return thumbnail.getAttribute('data-image-title');
 }
 
-function setDetailFromThumb(thumbnail) {
-    'use strict';
-    setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
+function setDetailsFromThumb(thumbnail){
+  'use strict';
+  setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
 }
 
-function addThumbClickHandler(thumb) {
-    'use strict';
-    thumb.addEventListener('click', function(event) {
-        event.preventDefault();
-        setDetailFromThumb(thumb);
-        showDetails();
-    });
+function addThumbClickHandler(thumb, index){
+  'use strict';
+  thumb.addEventListener('click', function (event){
+    event.preventDefault();
+    setDetailsFromThumb(thumb);
+    showDetails();
+    currentIndex = index;
+    console.log('Current index: ' + currentIndex);
+  });
 }
 
-function getThumbnailsArray () {
-    'use strict';
-    var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
-    // to copy thumbnails into an array called thumbnailArray.
-    var thumbnailArray = [].slice.call(thumbnails);
-    return thumbnailArray;
+function getThumbnailsArray(){
+  'use strict';
+  var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+  var thumbnailsArray = [].slice.call(thumbnails);
+  return thumbnailsArray;
 }
 
-function hideDetails() {
-    'use strict';
-    document.body.classList.add(HIDDEN_DETAIL_CLASS);
+// use the classList.add DOM method to manipulate the class name
+function hiddenDetails(){
+  'use strict';
+  document.body.classList.add(HIDDEN_DETAIL_CLASS);
 }
 
-function showDetails() {
-    'use strict';
-    var frame = document.querySelector(DETAIL_FRAME_SELECTOR);
-    document.body.classList.remove(HIDDEN_DETAIL_CLASS);
-    frame.classList.add(TINY_EFFECT_CLASS);
-    setTimeout(function () {
-        frame.classList.remove(TINY_EFFECT_CLASS);
-
-    }, 50);
+// keyCode is an integer, like 13 for Return, 32 for the space bar, and 38 for the up arrow.
+function addKeyPressHandler(){
+  'use strict';
+  document.body.addEventListener('keyup', function (event){
+    event.preventDefault();
+    console.log(event.keyCode);
+    if(event.keyCode === ESC_KEY){
+      hiddenDetails();
+    }
+  });
 }
 
-function addKeyPressHandler() {
-    'use strict';
-    document.body.addEventListener('keyup', function(event) {
-        event.preventDefault();
-        console.log(event.keyCode);
-        if (event.keyCode === ESC_KEY) {
-            hideDetails();
-        }
-    });
+function showDetails(){
+  'use strict';
+  document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+}
+
+// cycle back through thumbnails
+function preButtonClick(thumbnails){
+  'use strict';
+  preButton.addEventListener("click", function(event){
+    event.preventDefault();
+    if(currentIndex === 0){
+      currentIndex = thumbLength - 1;
+    }
+    else {
+      currentIndex--;
+    }
+    setDetailsFromThumb(thumbnails[currentIndex]);
+  });
+}
+
+// cycle forth through thumbnails
+function nextButtonClick(thumbnails){
+  'use strict';
+  nextButton.addEventListener("click", function(event){
+    event.preventDefault();
+    if(currentIndex === thumbLength - 1){
+      currentIndex = 0;
+    }
+    else {
+      currentIndex++;
+    }
+    setDetailsFromThumb(thumbnails[currentIndex]);
+  });
 }
 
 function initializeEvents() {
-    'use strict';
-    var thumbnails = getThumbnailsArray();
-    thumbnails.forEach(addThumbClickHandler);
-    addKeyPressHandler();
+  'use strict';
+  var thumbnails = getThumbnailsArray();
+  thumbLength = thumbnails.length; // get thumnails length
+  thumbnails.forEach(addThumbClickHandler); //addThumbClickHandler(thumbnails[1]);
+  addKeyPressHandler();
+  preButtonClick(thumbnails);
+  nextButtonClick(thumbnails);
 }
 
 initializeEvents();
